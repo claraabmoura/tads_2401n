e7c42fb330651cf3b7984195be85ceae2d728e9b
Clara Moura <152560314+claraabmoura@users.noreply.github.com>
2024-09-19 12:55:55 +0000

    enviando meu projeto

---
+++ draft.py
@@ -0,0 +1,20 @@
+from functions.insert_rows import insert_one_row
+from functions.create_table import create_table
+
+
+create_table(
+    database='mydatabase',
+    table_name='produtos',
+    columns_desc="""
+        id_produto INTEGER PRIMARY KEY,
+        nome CHAR NOT NULL,
+        qtd INTEGER NOT NULL
+    """
+)
+
+insert_one_row(
+   database_name='mydatabase',
+    table_name='produtos',
+    columns_name='nome, qtd',
+    values="'Xbox', 90"
+)
